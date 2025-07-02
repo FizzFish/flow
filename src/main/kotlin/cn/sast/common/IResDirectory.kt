c445c1259757fc409aeae35fdc6b7baf1106f5c8
FizzFish <yeshawuya@163.com>
2025-07-02 13:42:23 +0800

    Clean up resource interfaces and utilities

--- src/main/kotlin/cn/sast/common/IResDirectory.kt
+++ src/main/kotlin/cn/sast/common/IResDirectory.kt
@@ -1,32 +1,13 @@
 package cn.sast.common
 
-public interface IResDirectory : IResource {
-   public val normalize: IResDirectory
-   public val absolute: IResDirectory
+/**
+ * Representation of a directory resource.
+ */
+interface IResDirectory : IResource {
+    override val normalize: IResDirectory
+    override val absolute: IResDirectory
 
-   public abstract fun expandRes(outPut: IResDirectory): IResDirectory {
-   }
+    fun expandRes(outPut: IResDirectory): IResDirectory
 
-   public abstract fun listPathEntries(glob: String = ...): List<IResource> {
-   }
-
-   // $VF: Class flags could not be determined
-   internal class DefaultImpls {
-      @JvmStatic
-      fun getZipLike(`$this`: IResDirectory): Boolean {
-         return IResource.DefaultImpls.getZipLike(`$this`);
-      }
-
-      @Throws(java/io/IOException::class)
-      @JvmStatic
-      fun deleteDirectoryRecursively(`$this`: IResDirectory) {
-         IResource.DefaultImpls.deleteDirectoryRecursively(`$this`);
-      }
-
-      @Throws(java/io/IOException::class)
-      @JvmStatic
-      fun deleteDirectoryContents(`$this`: IResDirectory) {
-         IResource.DefaultImpls.deleteDirectoryContents(`$this`);
-      }
-   }
+    fun listPathEntries(glob: String = "*"): List<IResource>
 }
