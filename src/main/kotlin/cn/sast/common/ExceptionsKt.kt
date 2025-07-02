fe39b0bf0893a24a87a2a0bb9492640a94ad6270
FizzFish <yeshawuya@163.com>
2025-07-02 13:36:51 +0800

    Fix CustomRepeatingTimer and exception helper

--- src/main/kotlin/cn/sast/common/ExceptionsKt.kt
+++ src/main/kotlin/cn/sast/common/ExceptionsKt.kt
@@ -2,17 +2,30 @@ package cn.sast.common
 
 import java.io.IOException
 
-public fun Throwable.checkCritical() {
-   if (`$this$checkCritical` is IOException) {
-      val var10000: java.lang.String = `$this$checkCritical`.getMessage();
-      if (var10000 != null && StringsKt.contains(var10000, "no space left", true)) {
-         `$this$checkCritical`.printStackTrace(System.err);
-         System.exit(2);
-         throw new RuntimeException("System.exit returned normally, while it was supposed to halt JVM.");
-      }
-   } else if (`$this$checkCritical` is OutOfMemoryError) {
-      `$this$checkCritical`.printStackTrace(System.err);
-      System.exit(10);
-      throw new RuntimeException("System.exit returned normally, while it was supposed to halt JVM.");
-   }
+/**
+ * Checks whether this throwable is considered critical. If the exception
+ * indicates that the disk space is exhausted or an out-of-memory situation
+ * occurs, the JVM will be terminated.
+ */
+fun Throwable.checkCritical() {
+    when (this) {
+        is IOException -> {
+            message?.let {
+                if (it.contains("no space left", ignoreCase = true)) {
+                    printStackTrace(System.err)
+                    System.exit(2)
+                    throw RuntimeException(
+                        "System.exit returned normally, while it was supposed to halt JVM."
+                    )
+                }
+            }
+        }
+        is OutOfMemoryError -> {
+            printStackTrace(System.err)
+            System.exit(10)
+            throw RuntimeException(
+                "System.exit returned normally, while it was supposed to halt JVM."
+            )
+        }
+    }
 }
